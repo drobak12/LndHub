b1f1e2ca81665941b702e5dc98d91b52f1802656
jose.castaneda <jose.castaneda@globant.com>
2022-06-24 11:05:09 -0500

    Swith to v2 notifications

--- controllers/api.js
+++ controllers/api.js
@@ -99,14 +99,14 @@ const subscribeInvoicesCallCallback = async function (response) {
 
     if(!response.type){
       //Lightningchat
-      await redis.rpush('invoice_paid_for_bot', JSON.stringify({
+      await redis.rpush('v2_invoice_paid_for_bot', JSON.stringify({
         user_id: user._userid, 
         amt_paid_sat:LightningInvoiceSettledNotification.amt_paid_sat, 
         time: Math.trunc(new Date().getTime() / 1000)
       })); 
       //end lightningchat
     } else if(response.type === 'bill_pay'){
-      await redis.rpush('invoice_paid_for_bot', JSON.stringify({
+      await redis.rpush('v2_invoice_paid_for_bot', JSON.stringify({
         user_id: user._userid, 
         amt_paid_sat:LightningInvoiceSettledNotification.amt_paid_sat, 
         time: Math.trunc(new Date().getTime() / 1000),
